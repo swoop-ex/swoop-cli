9d7da19751b16f35741922843a6d28271df5db0f
Sebastian Johnsson <sebastian.johnsson@gmail.com>
2020-10-25 18:58:52 +0100

    Add support for looking up a specific pair/pool

--- tools/pairs/status.js
+++ tools/pairs/status.js
@@ -12,6 +12,11 @@ const argv = yargs
       description: 'The contract address for the UniswapV2Factory',
       type: 'string'
     })
+    .option('pair', {
+      alias: 'p',
+      description: 'The pair to check the pool status for, e.g. ONE/1BTC',
+      type: 'string'
+    })
     .help()
     .alias('help', 'h')
     .argv;
@@ -23,13 +28,20 @@ if (factoryAddress == null || factoryAddress == '') {
   process.exit(0);
 }
 
+var pair = null;
+if (argv.pair != null && argv.pair != '' && argv.pair.includes('/')) {
+  pair = argv.pair.split('/');
+}
+
 // Libs
 const web3 = require('web3');
 const { HmyEnv} = require("@swoop-exchange/utils");
 const { getAddress } = require("@harmony-js/crypto");
+const { parseTokens } = require("../shared/parseTokens");
 
 // Vars
 const network = new HmyEnv(argv.network);
+const tokens = parseTokens(network, 'all');
 
 const factoryContract = network.loadContract('@swoop-exchange/core/build/contracts/UniswapV2Factory.json', factoryAddress, 'deployer');
 const factoryInstance = factoryContract.methods;
@@ -45,15 +57,41 @@ const walletAddressBech32 = getAddress(walletAddress).bech32;
 
 async function status() {
   const length = await factoryInstance.allPairsLength().call(network.gasOptions());
-  console.log(`There is a total of ${length} pairs created by the factory ${factoryAddress} \n`)
+  console.log(`There is a total of ${length} pairs created by the factory ${factoryAddress} \n`);
 
-  if (length > 0) {
-    for (index = 0; index < length; index++) { 
-      console.log(`Fetching pair address for the token pair via index ${index} ...`);
-      pairAddress = await factoryInstance.allPairs(index).call(network.gasOptions());
-      console.log(`The pair address for the token pair at index ${index} is: ${pairAddress}\n`);
+  if (pair != null && pair.length == 2) {
+    const tokenASymbol = pair[0];
+    const tokenBSymbol = pair[1];
+
+    console.log(tokenASymbol)
+    console.log(tokenBSymbol)
+
+    const tokenAAddress = findToken(tokens, tokenASymbol);
+    const tokenBAddress = findToken(tokens, tokenBSymbol);
+
+    console.log(`Fetching pair address for the token pair ${tokenAAddress} / ${tokenBAddress} ...`);
+    let pairAddress = await factoryInstance.getPair(tokenAAddress, tokenBAddress).call(network.gasOptions());
+    console.log(`The pair address for the token pair ${tokenAAddress} / ${tokenBAddress} is: ${pairAddress}\n`);
+  } else {
+    if (length > 0) {
+      for (index = 0; index < length; index++) { 
+        console.log(`Fetching pair address for the token pair via index ${index} ...`);
+        pairAddress = await factoryInstance.allPairs(index).call(network.gasOptions());
+        console.log(`The pair address for the token pair at index ${index} is: ${pairAddress}\n`);
+      }
     }
   }
+
+}
+
+function findToken(tokens, name) {
+  let matches = tokens.filter(function(token) {
+    return token.symbol.toLowerCase() == name.toLowerCase();
+  });
+
+  const address = (matches && matches.length == 1) ? matches[0].address : null;
+
+  return address;
 }
 
 status()
